157921430e9cf9c25f991bef0f403bd787aaa301
BayuDC <dev.bayudc@gmail.com>
2023-07-22 05:55:21 +0700

    chore: setup fonts and colors

--- app.vue
+++ app.vue
@@ -2,9 +2,12 @@
     <Html>
         <Head>
             <Title>BayuDC</Title>
+            <Link rel="preconnect" href="https://fonts.googleapis.com" />
+            <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+            <Link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" />
         </Head>
     </Html>
-    <Body>
+    <Body class="text-light bg-dark">
         <h1>Hello World</h1>
     </Body>
 </template>
---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+    theme: {
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'],
+        },
+        colors: {
+            blue: '#3DB2FF',
+            blue2: '#0A9EFF',
+            light: '#FFFFFF',
+            dark: '#121212',
+        },
+    },
+    content: [],
+} as Config;
